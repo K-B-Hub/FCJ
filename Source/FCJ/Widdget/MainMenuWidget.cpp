1fdb9d1b4a85648db73bab6c9c4ae020363ee6d4
kms0917 <127091175+kms0917@users.noreply.github.com>
2025-08-27 19:01:36 +0900

    11

--- Source/FCJ/Widdget/MainMenuWidget.cpp
+++ Source/FCJ/Widdget/MainMenuWidget.cpp
@@ -2,4 +2,45 @@
 
 
 #include "MainMenuWidget.h"
+#include "Components/Button.h"
 
+void UMainMenuWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (LocalPlayButton)
+	{
+		LocalPlayButton->OnClicked.AddDynamic(this, &UMainMenuWidget::OnLocalPlayClicked);
+	}
+
+	if (MultiPlayButton)
+	{
+		MultiPlayButton->OnClicked.AddDynamic(this, &UMainMenuWidget::OnMultiPlayClicked);
+	}
+
+	if (SettingButton)
+	{
+		SettingButton->OnClicked.AddDynamic(this, &UMainMenuWidget::OnSettingClicked);
+	}
+
+	if (ExitButton)
+	{
+		ExitButton->OnClicked.AddDynamic(this, &UMainMenuWidget::OnExitClicked);
+	}
+}
+
+void UMainMenuWidget::OnLocalPlayClicked()
+{
+}
+
+void UMainMenuWidget::OnMultiPlayClicked()
+{
+}
+
+void UMainMenuWidget::OnSettingClicked()
+{
+}
+
+void UMainMenuWidget::OnExitClicked()
+{
+}
